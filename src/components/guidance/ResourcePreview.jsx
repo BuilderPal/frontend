ad9aa05a8be1c74ae315f70e698ff9c92faeb534
Izz Hafeez <izzhafeez@gmail.com>
2023-10-15 14:45:19 +0800

    Edit ResourcePreview.jsx: Add cards to materials

--- src/components/guidance/ResourcePreview.jsx
+++ src/components/guidance/ResourcePreview.jsx
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import { Popover, PopoverContent, PopoverTrigger } from 'components/ui/popover';
+import ResourceReporting from './ResourceReporting';
 
 class ResourcePreview extends Component {
   constructor(props) {
@@ -7,9 +9,14 @@ class ResourcePreview extends Component {
   }
 
   render() {
-    return <div className='flex w-full bg-nusb-light rounded-lg p-4'>
-      <h3 className='text-lg my-auto'>{this.props.resource}</h3>
-    </div>;
+    return <Popover>
+      <PopoverTrigger className={`flex w-full bg-nusb-light rounded-lg p-4 hover:bg-nusb-v-light`}>
+        <h3 className='text-lg my-auto'>{this.props.resource}</h3>
+      </PopoverTrigger>
+      <PopoverContent>
+        <ResourceReporting resource={this.props.resource}/>
+      </PopoverContent>
+    </Popover>;
   }
 }
 

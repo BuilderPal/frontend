fb2f6f033891bba7a2648c392abcab098f6bb7c2
Izz Hafeez <izzhafeez@gmail.com>
2023-10-15 13:33:56 +0800

    Edit InstructionNavigationPopover.jsx: First commit

---
+++ src/components/guidance/InstructionNavigationPopover.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class InstructionNavigationPopover extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>
+      Instruction
+    </div>;
+  }
+}
+
+export default InstructionNavigationPopover;

0acb7586f1e3e3b308442e4e0a71587ed3f056df
Hafeez <izzhafeez@gmail.com>
2023-10-16 18:02:00 +0800

    Add audio input

--- src/components/chat/ChatInput.jsx
+++ src/components/chat/ChatInput.jsx
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 import { Textarea } from 'components/ui/textarea';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from 'components/ui/tabs';
+import VoiceRecorder from './VoiceRecorder';
 
 class ChatInput extends Component {
   constructor(props) {
@@ -9,7 +11,18 @@ class ChatInput extends Component {
 
   render() {
     return <div className='p-4'>
-      <Textarea placeholder='hello' className='text-black'/>
+      <Tabs defaultValue='text'>
+        <TabsList>
+          <TabsTrigger value='text'>Text</TabsTrigger>
+          <TabsTrigger value='audio'>Audio</TabsTrigger>
+        </TabsList>
+        <TabsContent value='text'>
+          <Textarea placeholder='hello' className='text-black'/>
+        </TabsContent>
+        <TabsContent value='audio'>
+          <VoiceRecorder/>
+        </TabsContent>
+      </Tabs>
     </div>;
   }
 }

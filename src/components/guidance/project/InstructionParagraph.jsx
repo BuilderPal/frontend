0c5d4e94e8c4f6de4194e913c3dd0e07d4c454db
sherrpass <sherwinpoh3@gmail.com>
2023-11-06 20:55:19 +0800

    facelift

--- src/components/guidance/project/InstructionParagraph.jsx
+++ src/components/guidance/project/InstructionParagraph.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const InstructionParagraph = ({ paragraph }) => {
-  return (<p className='mt-2'>{paragraph}</p>)
+  return (<p className='mt-2 text-lg'>{`${paragraph}\n`}</p>)
 }
 
 export default InstructionParagraph
